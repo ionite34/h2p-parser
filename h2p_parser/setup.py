b0313676ac93060d742137aba4f6cb67b572be82
Ionite <ionite@pm.me>
2022-04-27 01:32:13 -0400

    Create setup.py

---
+++ h2p_parser/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='h2p_parser',
+    version='1.0.0',
+    packages=[''],
+    package_dir={'': 'h2p_parser'},
+    url='https://github.com/ionite34/h2p-parser',
+    license='Apache License, Version 2.0',
+    author='ionite',
+    author_email='me@ionite.io',
+    description='Heteronym to Phoneme Parser'
+)
