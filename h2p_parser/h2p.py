1ed0b16c5ff756b28279e031a55b2f978185c8dc
Ionite <ionite@pm.me>
2022-04-27 11:52:17 -0400

    Added preload ability to h2p init

--- h2p_parser/h2p.py
+++ h2p_parser/h2p.py
@@ -28,9 +28,25 @@ def format_ph(target, replacement, text):
 
 
 class H2p:
-    def __init__(self, dict_path=None):
+    def __init__(self, dict_path=None, preload=False):
+        """
+        Creates a H2p parser
+
+        :param dict_path: Path to a heteronym dictionary json file. Built-in dictionary will be used if None
+        :type dict_path: str
+        :param preload: Preloads the tokenizer and tagger during initialization
+        :type preload: bool
+        """
         self.dict = Dictionary(dict_path)
         self.tokenize = TweetTokenizer().tokenize
+        if preload:
+            self.preload()
+
+    # Method to preload tokenizer and pos_tag
+    def preload(self):
+        tokens = self.tokenize('a')
+        assert tokens == ['a']
+        assert pos_tag(tokens)[0][0] == 'a'
 
     # Method to check if a text line contains a heteronym
     def contains_het(self, text):
