6aeecfbf17369b995d53f49a529ecb6e7a049ad3
Ionite <ionite@pm.me>
2022-04-27 11:01:29 -0400

    Improved speed of filter text accent strip

--- h2p_parser/filter.py
+++ h2p_parser/filter.py
@@ -1,12 +1,18 @@
-import unicodedata
+from unicodedata import normalize
 import re
 
 
 # Filters text before parsing
-def filter_text(text):
+# @param text: text to be filtered
+# @return: filtered text
+def filter_text(text: str) -> str:
+    """
+    Filters text before parsing
+    :param text: Input raw text
+    :return: Text after stripped accents, lower-cased, and invalid punctuation removed
+    """
     # Strip accents
-    text = ''.join(char for char in unicodedata.normalize('NFD', text)
-                   if unicodedata.category(char) != 'Mn')
+    text = normalize('NFD', text)
     # To lowercase
     text = text.lower()
     # Remove all invalid punctuation
