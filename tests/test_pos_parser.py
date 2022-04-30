2dca6403754fb064477008ddfacb03b2e7edd3e2
Ionite <ionite@pm.me>
2022-04-30 02:33:39 -0400

    Test refactors

--- tests/test_pos_parser.py
+++ tests/test_pos_parser.py
@@ -14,17 +14,17 @@ class Test(TestCase):
         # Loop for each verb pos tag
         for verb_pos_tag in verb_pos_tags:
             actual = pos_parser.get_parent_pos(verb_pos_tag)
-            assert actual == 'VERB'
+            self.assertEqual('VERB', actual)
 
         # Loop for each noun pos tag
         for noun_pos_tag in noun_pos_tags:
             actual = pos_parser.get_parent_pos(noun_pos_tag)
-            assert actual == 'NOUN'
+            self.assertEqual('NOUN', actual)
 
         # Loop for each adverb pos tag
         for adverb_pos_tag in adverb_pos_tags:
             actual = pos_parser.get_parent_pos(adverb_pos_tag)
-            assert actual == 'ADVERB'
+            self.assertEqual('ADVERB', actual)
 
         # If the pos tag is not in the list, expect None
-        assert pos_parser.get_parent_pos('XYZ') is None
+        self.assertIsNone(pos_parser.get_parent_pos('XYZ'))
