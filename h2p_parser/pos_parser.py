e217930ad13f5407108918413c9d62e4e5b55ded
Ionite <ionite@pm.me>
2022-04-30 02:33:33 -0400

    Added type hints and control flow changes

--- h2p_parser/pos_parser.py
+++ h2p_parser/pos_parser.py
@@ -1,12 +1,17 @@
 # Part of Speech Tag Operations
 
 # Method to get the parent part of speech (VERB) or (NOUN) from a pos tag
-def get_parent_pos(pos):
-    if pos.startswith("VB"):
-        return "VERB"
-    elif pos.startswith("NN"):
-        return "NOUN"
-    elif pos.startswith("RB"):
-        return "ADVERB"
+from __future__ import annotations
+
+
+def get_parent_pos(pos: str) -> str | None:
+    # Get the parent part of speech from a pos tag
+    if pos.startswith('VB'):
+        return 'VERB'
+    elif pos.startswith('NN'):
+        return 'NOUN'
+    elif pos.startswith('RB'):
+        return 'ADVERB'
     else:
         return None
+
