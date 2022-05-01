c34861b26203ad5a8210f376e4873179edf36d2a
Ionite <ionite@pm.me>
2022-05-01 13:44:28 -0400

    Optimized util flow

--- h2p_parser/__main__.py
+++ h2p_parser/__main__.py
@@ -1,74 +1,86 @@
-# __main__.py
-
-import asyncio
 from InquirerPy import inquirer
 from InquirerPy.base.control import Choice
+from InquirerPy.validator import PathValidator
 from h2p_parser.utils import converter
 
 
-async def convert(file_type, input_file, output_file):
+def convert_h2p(input_file, output_file, delimiter):
     """
-    Converts a dictionary file from one format to another.
+    Converts a h2p dictionary file from one format to another.
     """
-    if file_type == 'h2p_dict':
-        await asyncio.run(converter.bin_delim_to_json(input_file, output_file))
-        print('Converted h2p_dict to json.')
-
-
-async def entry():
-    """
-    Prints help information.
-    """
-    # Select action type
-    action = await inquirer.select(
-        message='Select action:',
-        choices=[
-            "Convert delimited h2p_dict to json",
-            Choice(value=None, name='Exit')
-        ],
-        default=0,
-    ).execute_async()
-
-    if not action:
-        return
-
-    # Select input file
-    input_file = await inquirer.filepath(
-        message='Select input file:'
-    ).execute_async()
-
-    if not input_file:
-        return
-
-    # Select output file
-    output_file = await inquirer.filepath(
-        message='Select output file:'
-    ).execute_async()
-
-    if not output_file:
-        return
-
-    # Ask for delimiter
-    delimiter = await inquirer.text(
-        message='Enter delimiter:',
-        default='|'
-    ).execute_async()
-
-    if not delimiter:
-        return
-
-    # Run Process
     converter.bin_delim_to_json(input_file, output_file, delimiter)
     print('Converted h2p_dict to json.')
 
 
-def main():
+def prompt_action() -> str:
+    action = inquirer.select(
+        message='Select action:',
+        choices=[
+            "Convert",
+            Choice(value=None, name='Exit')
+        ],
+        default=0,
+    ).execute()
+    if not action:
+        exit(0)
+    return action
+
+
+def prompt_f_input():
     """
-    Main function for the h2p_parser package.
+    Prompts for input file.
     """
-    # check cli arguments
-    pass
+    return inquirer.filepath(
+        message='Select input file:',
+        validate=PathValidator(is_file=True, message='Input must be a file.')
+    ).execute()
+
+
+def prompt_f_output():
+    """
+    Prompts for output file.
+    """
+    return inquirer.filepath(
+        message='Select output file:',
+        validate=PathValidator(is_file=True, message='Output must be a file.')
+    ).execute()
+
+
+def action_convert():
+    """
+    Converts a h2p dictionary file from one format to another.
+    """
+    # Select input file
+    input_file = prompt_f_input()
+    if not input_file:
+        return
+
+    # Select output file
+    output_file = prompt_f_output()
+    if not output_file:
+        return
+
+    # Ask for delimiter
+    delimiter = inquirer.text(
+        message='Enter delimiter:',
+        default='|'
+    ).execute()
+    if not delimiter:
+        return
+
+    # Run Process
+    convert_h2p(input_file, output_file, delimiter)
+
+
+def entry():
+    """
+    Prints help information.
+    """
+    # Select action type
+    action = prompt_action()
+    if action == 'Convert':
+        action_convert()
 
 
 if __name__ == "__main__":
-    asyncio.run(entry())
+    entry()
