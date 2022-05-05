7ab273e53595e14f82502d8f4e7be565dd6c9b23
Ionite <ionite@pm.me>
2022-05-05 12:18:22 -0400

    Cache Tests

---
+++ tests/test_dict_cache.py
@@ -0,0 +1,21 @@
+import pytest
+from h2p_parser import dict_cache
+
+
+def test_dict_cache():
+    # Create dict cache instance
+    cache = dict_cache.DictCache()
+    assert isinstance(cache, dict_cache.DictCache)
+
+
+def test_dict_cache_add():
+    cache = dict_cache.DictCache()
+    # Add item to cache
+    cache.add('JARL', 'Y AA1 R L')
+    assert cache._cache['JARL'][0] == 'Y AA1 R L'
+    assert cache.get('JARL') == ('Y AA1 R L', None, False)
+
+
+def test_dict_cache_get():
+    cache = dict_cache.DictCache()
+    assert cache.get('JARL')[0] == 'Y AA1 R L'
